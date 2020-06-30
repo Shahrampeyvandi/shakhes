13c101b445750e0352da4a056e8953993c42838b
shahram peyvandi <yasfuny@gmail.com>
2020-06-30 23:12:51 +0430

    fsf

--- resources/views/Education/show.blade.php
+++ resources/views/Education/show.blade.php
@@ -1,21 +1,30 @@
-@extends('layout.temp')
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{route('BaseUrl')}}/vendor/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="{{route('BaseUrl')}}/vendor/bootstrap/RTL.css">
+    <link rel="stylesheet" href="{{route('BaseUrl')}}/vendor/FontAwesome/all.css">
+</head>
 
-<div class="container-fluid panel-table mt-5">
-   <div>
-   <img width="100%" src="{{$education->image}}" alt="">
-   </div>
-   <div class="desciption">
-   <h3 class="mt-5 mb-3">{{$education->title}}</h3>
-    {!! $education->description !!}
-   </div>
+<body>
+    <div class="container-fluid panel-table mt-5">
+    <div class="card">
+        <div class="card-body">
+           @if($education->image)
+            <div>
+                <img width="100%" src="{{$education->image}}" alt="">
+            </div>
+           @endif
+            <div class="desciption">
+            <h3 class="{{$education->image ? 'mt-5' : 'mt-1'}} mb-3">{{$education->title}}</h3>
+                {!! $education->description !!}
+            </div>
+        </div>
+    </div>
 </div>
-@endsection
-
-@section('css')
-    <style>
-       .desciption img{
-            width:100% !important;
-        }
-    </style>
-@endsection
+</body>
+</html>
