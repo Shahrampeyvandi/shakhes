6b66c3e5d6412190c7c84b2fe15f4a8594b6083b
shahram peyvandi <yasfuny@gmail.com>
2020-07-02 00:17:00 +0430

    234

--- resources/views/Education/show.blade.php
+++ resources/views/Education/show.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>شاخص - {{$education->title}}</title>
     <link rel="stylesheet" href="{{route('BaseUrl')}}/vendor/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="{{route('BaseUrl')}}/vendor/bootstrap/RTL.css">
     <link rel="stylesheet" href="{{route('BaseUrl')}}/vendor/FontAwesome/all.css">
